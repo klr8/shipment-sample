00d6c1ee0778d77242e5f3abbcd12c68f49c2504
Erwin Vervaet <erwin.vervaet@kbc.be>
2021-08-06 08:01:43 +0200

    Add notice about prereqs

--- monolith/build-and-run.sh
+++ monolith/build-and-run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+echo "-----------------------------------------------------------"
+echo "Building and running monolith"
+echo "Make sure you've got the following prerequisites installed:"
+echo "  - Java 11"
+echo "  - Maven 3.x"
+echo "  - Docker"
+echo "-----------------------------------------------------------"
+
 mvn clean package
 docker build -t shipment-monolith:latest .
 docker run -p 8080:8080 -t shipment-monolith:latest
